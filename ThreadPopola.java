aba1faf39d53212dfecd999690633f194d4d4160
fradevi02 <104077986+fradevi02@users.noreply.github.com>
2023-12-20 21:32:44 +0100

    Update ThreadPopola.java

--- ThreadPopola.java
+++ ThreadPopola.java
@@ -1,3 +1,9 @@
+/*
+Cermisoni Marco, MATRICOLA 748739, VA
+Oldani Marco, MATRICOLA 748243, VA
+De Vito Francesco, MATRICOLA 749044, VA
+Auteri Samuele, MATRICOLA 749710, VA
+*/
 package EmotionalSongs;
 
 import Database.Database;
@@ -7,20 +13,30 @@ import java.io.IOException;
 import java.sql.SQLException;
 import static EmotionalSongs.Brani.popola;
 
+/**
+ * Class to populate the database
+ * @author Auteri Samuele
+ */
 public class ThreadPopola extends Thread {
 
 
     private final String user;
     private final String pwd;
 
+    /**
+     * Constructor of the class ThreadPopola
+     * @param user A parameter of type String that expresses the username
+     * @param pwd A parameter of type String that expresses the password
+     */
     public ThreadPopola(String user, String pwd){
         this.user = user;
         this.pwd = pwd;
         this.start();
     }
 
-
-
+    /**
+     * Run method that puts the thread in ready state
+     */
     @Override
     public void run() {
         Database.setUser(user);
