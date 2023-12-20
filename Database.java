74fb5836bb4b527dc4742b0411d2b8b803ad85f2
fradevi02 <104077986+fradevi02@users.noreply.github.com>
2023-12-20 21:25:23 +0100

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,14 +1,21 @@
-package Database;/*
+/*
 Cermisoni Marco, MATRICOLA 748739, VA
 Oldani Marco, MATRICOLA 748243, VA
 De Vito Francesco, MATRICOLA 749044, VA
 Auteri Samuele, MATRICOLA 749710, VA
 */
+package Database;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.sql.*;
 
+/**
+ * Class for setting up and using the database
+ * @author Auteri Samuele
+ * @author Cermisoni Marco
+ * @author Oldani Marco
+ */
 public class Database implements Serializable{
     @Serial
     private static final long serialVersionUID = 1L;
@@ -18,8 +25,6 @@ public class Database implements Serializable{
     private final String host = "localhost/";
     private final String db_name = "EmotionalSongs";
     private final String url = protocol + host + db_name;
-    // private final String user = "postgres";
-    // private final String password = "admin";
     private static String user;
     private static String password;
 
@@ -28,6 +33,10 @@ public class Database implements Serializable{
     private static Connection connection;
     private static Statement statement;
 
+    /**
+     * Constructor of the class Database
+     * @throws SQLException Exception that occurs in Java when there's an error while working with a database using SQL (Structured Query Language) operations; SQL exceptions are typically thrown when there are issues such as: connection errors, syntax errors, constraint violations, data type mismatches, transaction issues, deadlocks and resource exhaustion.
+     */
     public Database() throws SQLException {
         connection = DriverManager.getConnection(url, user, password);
         statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
@@ -40,12 +49,20 @@ public class Database implements Serializable{
         return database;
     }
 
+    /**
+     * Method to set the username
+     * @param u A parameter of type String that expresses the username
+     */
     public static void setUser(String u){
-        user=u;
+        user = u;
     }
 
+    /**
+     * Method to set the password
+     * @param p A parameter of type String that expresses the password
+     */
     public static void setPassword(String p){
-        password=p;
+        password = p;
     }
 
     public static Statement getStatement() {
