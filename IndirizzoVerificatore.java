4ce8d70371d7ed89e65c265cb0f0a1bee1222cbf
fradevi02 <104077986+fradevi02@users.noreply.github.com>
2023-12-20 21:26:09 +0100

    Update IndirizzoVerificatore.java

--- IndirizzoVerificatore.java
+++ IndirizzoVerificatore.java
@@ -1,11 +1,26 @@
+/*
+Cermisoni Marco, MATRICOLA 748739, VA
+Oldani Marco, MATRICOLA 748243, VA
+De Vito Francesco, MATRICOLA 749044, VA
+Auteri Samuele, MATRICOLA 749710, VA
+*/
 package EmotionalSongs;
 
 import java.util.regex.Pattern;
 
+/**
+ * Class that controls a user's physical address
+ * @author De Vito Francesco
+ */
 public class IndirizzoVerificatore {
     private static final Pattern CAP_PATTERN = Pattern.compile("^\\d{5}$");
     private static final Pattern NUMERO_CIVICO_PATTERN = Pattern.compile("^\\d+$");
 
+    /**
+     * Method that performs a series of checks on the correctness of the physical address provided by the user
+     * @param indirizzo A parameter of type String that expresses the user's address
+     * @return The method returns a boolean that represents whether the user-provided physical address is valid
+     */
     public static boolean isIndirizzoValid(String indirizzo) {
         String[] indirizzoParts = indirizzo.split(",");
 
@@ -22,7 +37,6 @@ public class IndirizzoVerificatore {
         if (!CAP_PATTERN.matcher(cap).matches() || !NUMERO_CIVICO_PATTERN.matcher(numeroCivico).matches()) {
             return false;
         }
-
         return true;
     }
 }
